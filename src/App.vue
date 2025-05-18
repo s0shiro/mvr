4f252171cf6a5d72270c96900de60892ac49854e
kaizen <mascarinas022@gmail.com>
2025-05-18 09:28:26 +0800

    feat: add Toaster and VueQueryDevtools components to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import { VueQueryDevtools } from '@tanstack/vue-query-devtools'
+import { Toaster } from '@/components/ui/sonner'
 </script>
 
 <template>
+  <Toaster />
+  <VueQueryDevtools />
   <RouterView />
 </template>
 
